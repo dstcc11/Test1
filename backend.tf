1b0d2136cbb4ae64479ac719ca8b6e2d0a69ca11
jf3079 <jerome3079@gmail.com>
2025-08-13 17:43:16 -0400

    updated

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket3079"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
